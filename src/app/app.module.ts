9587876e9442b3dae6bbd4668a8543002bb510e5
richie.buckingham <richie.buckingham@gmail.com>
2018-04-03 20:59:21 +1000

    got all the auth components working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
+import { AuthModule } from '../auth/auth.module';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +32,18 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+
+/* <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkGZyl6A6kx7VASNn3RdhMHbC3vLObq8c",
+    authDomain: "fitness-app-a9185.firebaseapp.com",
+    databaseURL: "https://fitness-app-a9185.firebaseio.com",
+    projectId: "fitness-app-a9185",
+    storageBucket: "fitness-app-a9185.appspot.com",
+    messagingSenderId: "129417252733"
+  };
+  firebase.initializeApp(config);
+</script> */
---
+++ src/auth/register/containers/register/register.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  template: `
+    <div>
+      Register
+      <auth-form></auth-form>
+    </div>
+  `
+})
+export class RegisterComponent {
+  constructor() {}
+}
